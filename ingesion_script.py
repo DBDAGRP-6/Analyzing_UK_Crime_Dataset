0677ea4e409c76e2710b80fad0da87e7d1f6a99e
shivam21ss <shivamsharma2111.ss@gmail.com>
2023-09-05 22:18:41 +0530

    adding final scripts

--- ingesion_script.py
+++ ingesion_script.py
@@ -22,23 +22,20 @@ connection_properties = {
 outcomes_df = spark.read.jdbc(url=jdbc_url, table="Crime_Outcomes", properties=connection_properties)
 outcomes_df1 = spark.read.csv("s3://raw-proj-files-19-23/outcomes/")
 outcomes = outcomes_df.union(outcomes_df1)
-outcomes.repartition(1).write.parquet("s3://grp-6-datalake/outcomes")
+outcomes.repartition(1).write.parquet("s3://grp6-datalakexxx/outcomes")
 
 street_df = spark.read.jdbc(url=jdbc_url, table="Street_Crimes", properties=connection_properties)
 street_df1 = spark.read.csv("s3://raw-proj-files-19-23/street/")
 street = street_df.union(street_df1)
-street.repartition(1).write.parquet("s3://grp-6-datalake/street")
+street.repartition(1).write.parquet("s3://grp6-datalakexxx/street")
 
 
 stop_n_search_df = spark.read.jdbc(url=jdbc_url, table="Stop_And_Search", properties=connection_properties)
 stop_n_search_df1 = spark.read.csv("s3://raw-proj-files-19-23/stop_and_search/")
 stop_n_search = stop_n_search_df.union(stop_n_search_df1)
-stop_n_search.repartition(1).write.parquet("s3://grp-6-datalake/stop_n_search")
+stop_n_search.repartition(1).write.parquet("s3://grp6-datalakexxx/stop_n_search")
 
 
 job = Job(glueContext)
 job.init(args['JOB_NAME'], args)
 job.commit()
-# this is for testing
-
-
