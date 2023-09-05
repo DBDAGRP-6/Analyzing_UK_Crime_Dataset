067271a0c2bfb670305e1869a5a8dcad65e397df
Vivek-Halpatrao <vivekhalpatrao96k@gmail.com>
2023-09-05 17:27:49 +0530

    Merging both transformation

--- transform.py
+++ transform.py
@@ -17,7 +17,7 @@ spark = glueContext.spark_session
  
 #path
 
-df_street = spark.read.parquet("s3://group6-project-database/street/",header=True,inferSchema=True)
+df_street = spark.read.parquet("s3://grp6-datalake/street/",header=True,inferSchema=True)
 
 
 #Dropping or filling null values
@@ -140,7 +140,7 @@ df_street= df_street.withColumn("Crime type", when(conditions[0][0], conditions[
 
 #----------------------------------------------------------outcome------------------------------------------------------------------------------
 
-df_outcome = spark.read.parquet("s3://group6-project-database/outcome/",header=True,inferSchema=True)
+df_outcome = spark.read.parquet("s3://grp6-datalake/outcomes/",header=True,inferSchema=True)
 
 #dropping columns which are already present in Street 
 temp=['Reported by','Falls within','Longitude','Latitude','Location','LSOA code','LSOA name']
@@ -182,7 +182,45 @@ joined_df_filled = joined_df.withColumn("Date", F.to_timestamp(
 #calculating difference between month of case & month of outcome
 joined_df_filled = joined_df_filled.withColumn("months_between", months_between(col("Date"), col("Month")))
 
-joined_df_filled.repartition(1).write.parquet('s3://gr6-data-ware/data/final')
+joined_df_filled.repartition(1).write.parquet('s3://grp6-dataware/final')
+
+#========================================================Stop and search====================================================
+
+from pyspark.sql.functions import col, to_timestamp, date_format,concat,lit, to_timestamp, from_utc_timestamp,when
+
+df =spark.read.parquet("s3://grp6-datalake/stop_n_search/",header=True,inferSchema=True)
+
+df=df.drop(*['Part of a policing operation','Policing operation','Officer-defined ethnicity','Outcome linked to object of search','Removal of more than just outer clothing'])
+
+df = df.fillna('Details not available', subset=['Outcome'])
+
+df = df.fillna('Not mentioned', subset=['Object of search'])
+
+df= df.fillna('Not mentioned', subset=['Legislation'])
+
+df= df.fillna('Other ethnic group - Not stated', subset=['Self-defined ethnicity'])
+
+df= df.fillna('0.0',subset=['Latitude'])
+
+df= df.fillna('0.0',subset=['Longitude'])
+
+df= df.fillna('Not mentioned', subset=['Age range'])
+
+df= df.fillna('Other', subset=['Gender'])
+
+df = df.withColumn("DateTime", from_utc_timestamp(to_timestamp("Date", "yyyy-MM-dd'T'HH:mm:ssXXX"), "UTC"))
+
+df = df.drop('Date')
+
+conditions = [
+(col("Outcome").isin('Suspect arrested','Suspect summonsed to court','Offender given penalty notice','Offender cautioned','Suspected psychoactive substances seized - No further action'), 'Found guilty'),
+(col("Outcome").isin('Nothing found - no further action','Details not available','Article found - Detailed outcome unavailable','Offender given drugs possession warning','Local resolution','Details not available'), 'Not Found guilty')
+]
+
+df= df.withColumn("Outcome type", when(conditions[0][0], conditions[0][1]).when(conditions[1][0], conditions[1][1]).otherwise('Other'))
+
+df.repartition(1).write.parquet("s3://grp6-dataware/stop")
+
 
 job = Job(glueContext)
 job.init(args['JOB_NAME'], args)
